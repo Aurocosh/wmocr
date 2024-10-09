5966851c2c74d7e20b154fdc7f93981a790d374d
Aurocosh <Aurocosh@gmail.com>
2024-10-09 23:12:32 +0700

    Added command line arguments

--- wm-ocr-cli/Program.cs
+++ wm-ocr-cli/Program.cs
@@ -10,18 +10,58 @@ using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Foundation;
 using BitmapDecoder = Windows.Graphics.Imaging.BitmapDecoder;
+using CommandLine;
+using Windows.UI.Xaml.Data;
 
 namespace wm_ocr_cli
 {
+    public class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Path to the image file to process with OCR.")]
+        public string Input { get; set; }
+
+        [Option('o', "output", Required = false, HelpText = "Path to output text file. Writes OCR result to this file.")]
+        public string Output { get; set; }
+
+        [Option('s', "stdout", Required = false, HelpText = "Perform OCR and print result to stdout. Default behaviour if no other output options is specified.")]
+        public string Stdout { get; set; }
+
+        [Option('c', "crop", Required = false, HelpText = "Path to cropped output image.Detect bounding box for all text and crop original image to this bounding box.")]
+        public string Crop { get; set; }
+
+        [Option('b', "bb-margin", Required = false, Default = 10, HelpText = "Bounding box margin.The bounding box will be expanded by this value.")]
+        public int BoundingBoxMargin { get; set; }
+
+        [Option('a', "append", HelpText = "When writing to text file append instead of overwriting the file.")]
+        public bool Append { get; set; }
+    }
+
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            string imagePath = "D:\\Temp\\test.jpg";
+            var result = await Parser.Default.ParseArguments<Options>(args)
+              .MapResult(
+                  options => Execute(options), // Execute and capture result
+                  errors => HandleErrors(errors)); // Handle errors
+
+            //Console.WriteLine($"retValue= {result}");
+        }
+
+        static async Task<int> Execute(Options options)
+        {
+            var fullImagePath = Path.GetFullPath(options.Input);
+            if (!File.Exists(fullImagePath))
+            {
+                Console.Error.WriteLine($"Provided input file does not exist.");
+                return 1;
+            }
+
+            //string imagePath = "D:\\Temp\\test.jpg";
 
             try
             {
-                var result = RecognizeAsync(imagePath, "ru").GetAwaiter().GetResult();
+                var result = RecognizeAsync(fullImagePath, "ru").GetAwaiter().GetResult();
 
                 if (result != null)
                 {
@@ -38,8 +78,22 @@ namespace wm_ocr_cli
             catch (Exception error)
             {
                 Console.Error.WriteLine("Error: " + error.Message);
+                return 1;
             }
             Console.ReadLine();
+            return 0;
+        }
+
+
+        // Handle parsing errors
+        private static Task<int> HandleErrors(IEnumerable<Error> errors)
+        {
+            foreach (var error in errors)
+            {
+                Console.Error.WriteLine($"Error: {error}");
+            }
+            //Environment.Exit(1); // Exit with error code
+            return Task.FromResult(1);
         }
 
         static async Task<SoftwareBitmap> LoadImageAsync(StorageFile file)
